2d2fe948617c447f44b7671fd0164f0507e4934c
ezpheliarmuz-uisrael <67877369+ezpheliarmuz-uisrael@users.noreply.github.com>
2022-06-13 22:23:20 -0500

    Menu
    Se creo el menu.

--- JavaS2Ejercicios/src/main/java/com/mycompany/javas2ejercicios/JavaS2Ejercicios.java
+++ JavaS2Ejercicios/src/main/java/com/mycompany/javas2ejercicios/JavaS2Ejercicios.java
@@ -5,13 +5,52 @@
 
 package com.mycompany.javas2ejercicios;
 
+import java.util.Scanner;
+
 /**
  *
  * @author victo
  */
 public class JavaS2Ejercicios {
-
+public static int num1,num2;
+public static Scanner scTeclado = new Scanner (System.in);
     public static void main(String[] args) {
         
+    }
+    //Menu
+    public static void MenuPrincipal(){
+    
+     
+     
+     int op;
+    
+        do { 
+            System.out.println("*******MENU PRINCIPAL******");
+            System.out.println("1: Suma");
+            System.out.println("2: Resta");
+            System.out.println("3: Multiplicación");
+            System.out.println("4: División ");
+            System.out.println("5: Salir");
+            System.out.println("Ingrese la opción del Menú");
+            
+            op= scTeclado.nextInt();
+            switch (op){
+               case 1:
+                   suma(num1, num2);
+                   break;
+               case 2:
+                   //Opcion2();
+                   break;
+             
+                default:
+                    System.out.println("Opción no valida");
+                    break;
+            }
+        } while (op!=5);
+    }//Fin menu
+    //suma
+    public static void suma(int numero1,int numero2){
+    int resultado= numero1+numero2; 
+    System.out.println("La suma es: "+ resultado);
     }
 }
